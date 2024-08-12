f601f13cd13a043dad139ddc809f0b268c59c339
Jonas <jonas.luetolf@outlook.com>
2024-08-12 14:55:05 +0200

    update network.py
    change handling of missing random_init method from try ... except to
    hasattr
    
    error handling of missing groups in hdf5 file or wrong format of hdf5
    file

--- neural_network/network.py
+++ neural_network/network.py
@@ -8,7 +8,7 @@ from .activation.activation import ActivationFunction
 
 class NeuralNetwork:
     def __init__(self, loss_function: LossFunction) -> None:
-        self.layers = []
+        self.layers: list[Layer | ActivationFunction] = []
         self.loss_function = loss_function
 
     def add_layer(self, layer: Layer | ActivationFunction) -> None:
@@ -16,10 +16,8 @@ class NeuralNetwork:
 
     def random_init(self) -> None:
         for layer in self.layers:
-            try:
+            if hasattr(layer, "random_init"):
                 layer.random_init()
-            except AttributeError:
-                pass
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         for layer in self.layers:
@@ -39,15 +37,23 @@ class NeuralNetwork:
                 self.backward(loss, lr)
 
     def save(self, path: str) -> None:
-        with h5py.File(path, 'w') as h5file:
+        with h5py.File(path, "w") as h5file:
             for i, layer in enumerate(self.layers):
                 if isinstance(layer, Layer):
-                    layer.save(f'layer_{i}', h5file)
+                    layer.save(f"layer_{i}", h5file)
 
     def load(self, path: str) -> None:
-        with h5py.File(path, 'r') as h5file:
+        with h5py.File(path, "r") as h5file:
             for i, layer in enumerate(self.layers):
                 if isinstance(layer, Layer):
-                    group = h5file[f'layer_{i}']
-                    assert isinstance(group, h5py.Group)
+                    group_name = f"layer_{i}"
+                    if group_name not in h5file:
+                        raise KeyError(f"Group {group_name} not found in the file.")
+
+                    group = h5file[group_name]
+
+                    if not isinstance(group, h5py.Group):
+                        raise ValueError(
+                            f"Expected Group, but found {type(group).__name__}"
+                        )
                     layer.load(group)
