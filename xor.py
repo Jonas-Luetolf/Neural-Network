f66247d8ce714002464fac0380ca9f10d69776f7
Jonas <jonas.luetolf@outlook.com>
2024-08-12 14:26:44 +0200

    update xor
    xor now uses NeuralNetwork class
    trained xor model get saved to xor.hdf5

--- xor.py
+++ xor.py
@@ -1,57 +1,53 @@
 import numpy as np
+import sys
 
 from neural_network.layers import DenseLayer
-from neural_network.activation import Tanh, ReLu
+from neural_network.activation import Tanh
 from neural_network.loss import MSE
+from neural_network.network import NeuralNetwork
+
+RAND = False
+PATH = "./xor.hdf5"
 
 
 def main():
-    mse = MSE()
-    network = [
-        DenseLayer(2, 3),
-        Tanh(),
-        DenseLayer(3, 3),
-        Tanh(),
-        DenseLayer(3, 1),
-        Tanh(),
-    ]
-    for layer in network:
-        try:
-            layer.random_init()
 
-        except:
-            pass
+    network = NeuralNetwork(MSE())
+    network.add_layer(DenseLayer(2, 3))
+    network.add_layer(Tanh())
+    network.add_layer(DenseLayer(3, 3))
+    network.add_layer(Tanh())
+    network.add_layer(DenseLayer(3, 1))
+    network.add_layer(Tanh())
 
-    inputs = np.array([[[0, 0]], [[0, 1]], [[1, 0]], [[1, 1]]])
-    outputs = np.array([[[0]], [[1]], [[1]], [[0]]])
-    print(outputs.shape)
-    print(inputs.shape)
-    for i in range(0, 1000):
-        for inp, out in zip(inputs, outputs):
-            x = inp
-            for layer in network:
-                x = layer.forward(x)
-            loss = mse.loss_prime(x, out)
-            print(loss)
-            for layer in reversed(network):
-                loss = layer.backward(loss, 0.1)
+    if RAND:
+        print(f"neural network gets trained and saved to {PATH}")
+        network.random_init()
+
+        inputs = np.array([[[0, 0]], [[0, 1]], [[1, 0]], [[1, 1]]])
+        outputs = np.array([[[0]], [[1]], [[1]], [[0]]])
+
+        network.train(inputs, outputs, 1000, 0.1)
+        network.save(PATH)
+
+    else:
+        print(f"neural network loaded from {PATH}")
+        network.load(PATH)
 
     x = np.array([[0, 1]])
-    for layer in network:
-        x = layer.forward(x)
-    print(np.around(x, 3))
+    y = network.forward(x)
+    print("1: ", np.around(y, 0))
 
     x = np.array([[1, 1]])
-    for layer in network:
-        x = layer.forward(x)
-    print(np.around(x, 3))
+    y = network.forward(x)
+    print("0: ", np.around(y, 0))
 
     x = np.array([[1, 0]])
-
-    for layer in network:
-        x = layer.forward(x)
-    print(np.around(x, 3))
+    y = network.forward(x)
+    print("1: ", np.around(y, 0))
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "train":
+        RAND = True
     main()
